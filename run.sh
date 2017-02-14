d085255220fb34590f6c3be8f92d91dd5aa8d706
Eric Rincon <rincon@cs.utexas.edu>
2017-02-14 10:41:37 -0600

    added sbatch args

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
-
+#SBATCH -J papi           # job name
+#SBATCH -o myPAPI.o%j       # output and error file name (%j expands to jobID)
+#SBATCH -n 16              # total number of mpi tasks requested
+#SBATCH -p development     # queue (partition) -- normal, development, etc.
+#SBATCH -t 01:30:00        # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=eric.rincon@utexas.edu
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
 module load papi
 git pull
 gcc main.cpp -O3 -std=c++11
