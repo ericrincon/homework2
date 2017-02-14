d2c92606a9ba32926a11a3813c19a3b01bf54dbd
Eric Rincon <rincon@cs.utexas.edu>
2017-02-14 10:52:00 -0600

    sbatch update

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 #SBATCH -p development     # queue (partition) -- normal, development, etc.
 #SBATCH -t 01:30:00        # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=eric.rincon@utexas.edu
+#SBATCH -A G-800722
 #SBATCH --mail-type=begin  # email me when the job starts
 #SBATCH --mail-type=end    # email me when the job finishes
 module load papi
